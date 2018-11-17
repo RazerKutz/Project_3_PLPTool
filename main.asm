97d66b4f8a8e12a8aeb686c1387866692d3d1be6
Anthony <anthonykutz57@gmail.com>
2018-11-16 19:36:03 -0800

    fixed

--- main.asm
+++ main.asm
@@ -4,7 +4,7 @@
 li $sp, 0x10fffffc
 
 li $t0, 0xf0000000 # 0(CommandBuffer), 4(statusBuffer), 8(reciveBuffer)
-li $t5, 0b0100000 # Bit mask to Cheak Upper.
+li $t5, 0b0010000 # Bit mask to Cheak Upper.
 
 li $t9, 0b10 # Bit mask for Status Buffer
 
@@ -54,25 +54,25 @@ main:
 	receive_character:
 		lw $t3, 8($t0) # $t3 holds the actual Character.
 		and $t2, $t3, $t5 # using masking to see if its upper case letter.
-		beq $t2, $0 convert_to_lower # If upper case convert to lower.
+		beq $t2, $t5 clear_status # If upper case convert to lower.
 		nop
 		j write_to_array
 		nop
 
 	convert_to_lower:
-		addiu $t3, $t3, 32
+		addiu $t3, $t3, -32
 		j write_to_array
 		nop
 
 	write_to_array:
 		sw $t3, 0($s0)
-		addiu $s0, $s0, 4
 		j clear_status
 		nop
 
 	clear_status:
 		# 3. Then send the command buffer a clear command so the UART sends the next
 		# Character.
+		addiu $s0, $s0, 4
 		sw $t9, 0($t0)
 		# 4. Loop
 		j read_UART
